542d5fbea4245561fd34f905fb7f3a57795e28fa
Rahul Sharma <rahulgaur116@gmail.com>
2024-03-05 21:27:34 -0500

    Added revised fetch function including if statement to loop thru the aqai pollutants data

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 
-
-
 var myapikey = "f09a8b987378f5c12242add6b4dd73f6243f9a8b";
 var locationinput = $("#location");
 var searchbtn = $("#search");
-var aqi = $("#aqi");
-var pm25 = $("#pm25");
+var pollutant1 = $("#pol1");
+var pollutant2 = $("#pol2");
+var pollutant3 = $("#pol3");
 
 // Event listener for search button click
 searchbtn.on("click", function() {
     var location = locationinput.val();
-    console.log(location)
+    console.log(location);
 
     // Fetch current overall AQI data using Waqi API
     fetch(`https://api.waqi.info/feed/${location}/?token=${myapikey}`)
     .then(function(resp) {
+        if (!resp.ok) {
+            throw new Error('Network response was not ok');
+        }
         return resp.json();
     })
     .then(function(data) {
-        console.log(data);
-        aqi.text(data.data.aqi);
+        console.log(data.data);
+
+        if (data.data && data.data.iaqi) {
+            displayPollutant("pm25", pollutant1, data.data.iaqi);
+            displayPollutant("so2", pollutant2, data.data.iaqi);
+            displayPollutant("no2", pollutant3, data.data.iaqi);
+        }
+    })
+    .catch(function(error) {
+        console.error('Error fetching data:', error);
     });
-    
-     // Fetch current PM25 data using Waqi API
-     fetch(`https://api.waqi.info/feed/${location}/?token=${myapikey}`)
-     .then(function(resp) {
-         return resp.json();
-     })
-     .then(function(data) {
-         console.log(data);
-         pm25.text(data.data.iaqi.pm25.v);
-     });
 });
 
+function displayPollutant(pollutantKey, element, iaqiData) {
+    if (iaqiData[pollutantKey]) {
+        element.text(iaqiData[pollutantKey].v);
+    } else {
+        element.text(pollutantKey + " Data Not Available");
+    }
+}
